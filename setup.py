0f277f47ab48a98b544ce9a322c54aa063d417c8
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-11-23 22:19:10 -0500

    [minor] reformat `setup.py`

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 
 from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension, CUDA_HOME
-has_cuda = (torch.cuda.is_available() and CUDA_HOME is not None) or os.getenv("FORCE_CUDA", "0") == "1"
+has_cuda = (torch.cuda.is_available() and CUDA_HOME is not None) or os.getenv('FORCE_CUDA', '0') == '1'
 
 from torchsparse import __version__
 
