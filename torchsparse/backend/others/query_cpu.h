a249463504b391bedb2a0d9fc8430968ec2d993e
iaheung <iaheung@davidson.edu@phy1kuchera.ada.davidson.edu>
2023-10-23 15:31:26 -0400

    deleted irrelevant directories

--- torchsparse/backend/others/query_cpu.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <torch/torch.h>
-
-at::Tensor hash_query_cpu(const at::Tensor hash_query,
-                          const at::Tensor hash_target,
-                          const at::Tensor idx_target);
--- torchsparse/backends.py
+++
@@ -1,3 +0,0 @@
-def init():
-    global benchmark
-    benchmark = False
